be16b4b179ec9483dbb7f6c9dbc7cbea071ef437
Yoon Young <yoonyouk@andrew.cmu.edu>
2019-10-30 13:03:03 -0400

    HW 6B
    adding in the implementation of the shopping cart features
    attempts to create the carousel feature in the product detail pages

--- script.js
+++ script.js
@@ -1,61 +1,152 @@
 var quantity;
 var filling;
 var additional;
+var value;
 
+var cartArray = [];
+
+
+//create object
+function bunItem(filling, quantity){
+    this.filling = filling;
+    this.quantity = quantity;
+}
 
 //submit to cart
 function incrementValue() {
-    "use strict";
-    var value = parseInt(document.getElementById('cartqty').value, 10); 
-    value++; //adding for each click
-    document.getElementById('cartqty').value = value;
+    
+    if(quantity !== null && filling !== null){
+        var value = /*parseInt(document.getElementById('cartqty').value, 10); 
+    value++; */
+            //adding for each click
+        
+        cartArray.push([bunItem.filling, bunItem.quantity]);
+    
+        document.getElementById('cartqty').value = cartArray.length;
+    
+        localStorage.setItem("cartArray", JSON.stringify(cartArray));
+        
+        console.log(cartArray);
+    }
+
 }
 
+function getCart(){
+    var cart = JSON.parse(localStorage.getItem("cartArray"));
+   console.log(cart);
+    
+    document.getElementById("cartFull").innerHTML = "";
+    for(var i = 0; i<cart.length; i++){
+        
+        var bunType = document.createElement("p");
+        var typeText = document.createTextNode=cart[i][0];
+        bunType.append("Blackberry");
+        
+        var item = document.getElementById("cartFull");
+        item.append("Blackberry");
+        
+        
+        
+        var fillingHTML = document.createElement("p");
+         var fillingText = document.createTextNode=cart[i][0];
+        
+        fillingHTML.append(fillingText);
+        var item = document.getElementById("cartFull");
+        item.append(fillingHTML);
+        
+        
+        
+        var quantityHTML = document.createElement("p");
+            var quantityText= document.createTextNode = cart[i][1];
+        quantityHTML.append(quantityText);
+        item.append(quantityHTML);
+        
+        var button = document.createElement("input");
+        button.setAttribute("type", "submit");
+        button.setAttribute("onclick", "removeFromCart(this);");
+        button.setAttribute("value", "Remove");
+        button.setAttribute("value2", i);
+        item.append(button);
+        
+
+    }
+}
+
+function removeFromCart(itemToRemove){
+var cart = JSON.parse(localStorage.getItem("cartArray"));
+    var removeIndex = itemToRemove.getAttribute("value2");
+    cart.splice(removeIndex , 1);
+    localStorage.setItem("cartArray", JSON.stringify(cart));
+    getCart();
+}
 
 function qty1(){
-    quantity = document.getElementById("opt1").value;
+    bunItem.quantity = document.getElementById("opt1").value;
 
 }
 
 function qty2(){
-    quantity = document.getElementById("opt2").value;
+    bunItem.quantity = document.getElementById("opt2").value;
 }
 
 
 function qty3(){
-    quantity = document.getElementById("opt3").value;
+    bunItem.quantity = document.getElementById("opt3").value;
 }
 
-
 function qty4(){
-    quantity = document.getElementById("opt4").value;
+    bunItem.quantity = document.getElementById("opt4").value;
 }
 
 function fill1(){
-    filling = document.getElementById("opt5").value; 
+    bunItem.filling = document.getElementById("opt5").value; 
 }
 
 function fill2(){
-    filling = document.getElementById("opt6").value;   
+    bunItem.filling = document.getElementById("opt6").value;   
 }
 
 function fill3(){
-    filling = document.getElementById("opt7").value;   
+    bunItem.filling = document.getElementById("opt7").value;   
 }
 
 function fill4(){
-    filling = document.getElementById("opt8").value; 
+    bunItem.filling = document.getElementById("opt8").value; 
 }
 
 function fill5(){
-    filling = document.getElementById("opt9").value; 
+    bunItem.filling = document.getElementById("opt9").value; 
 }
 
-function validate(){
-    if(quantity != null & filling != null){
-        console.log(quantity +" "+ filling);
-    } 
-    else{
-        console.log("don't add");
-    }
+
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
